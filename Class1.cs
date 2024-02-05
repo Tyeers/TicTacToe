7d5bf6ec5d4978c6d5da3ed2ce877d6f3eee3dca
Tyeers <aqilapasya7@gmail.com>
2024-02-05 14:16:40 +0700

    first-commit

---
+++ Class1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe
+{
+    public class TicTacToeGame
+    {
+        private Button[,] buttons;
+        private char currentPlayer = 'X';
+        private int seconds = 0;
+        private System.Windows.Forms.Label label1;
+
+        public TicTacToeGame(Button[,] buttons, System.Windows.Forms.Label label1)
+        {
+            this.buttons = buttons;
+            this.label1 = label1;
+        }
+
+        public char CurrentPlayer
+        {
+            get { return currentPlayer; }
+        }
+
+        public System.Windows.Forms.Label Label1
+        {
+            get { return label1; }
+        }
+        public void InitializeGame(Button[,] buttons, System.Windows.Forms.Label label1)
+        {
+            this.buttons = buttons;
+            this.label1 = label1;
+        }
+
+        public void Button_Click(Button button)
+        {
+            if (button.Text == "" && !IsGameWon())
+            {
+                button.Text = currentPlayer.ToString();
+
+                if (IsGameWon())
+                {
+                    MessageBox.Show($"Player {currentPlayer} wins!");
+                    ResetGame();
+                }
+                else if (IsBoardFull())
+                {
+                    MessageBox.Show("It's a draw!", label1.Text);
+                    ResetGame();
+                }
+                else
+                {
+                    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+                }
+            }
+        }
+
+        public bool IsGameWon()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if ((buttons[i, 0].Text == currentPlayer.ToString() && buttons[i, 1].Text == currentPlayer.ToString() && buttons[i, 2].Text == currentPlayer.ToString()) ||
+                    (buttons[0, i].Text == currentPlayer.ToString() && buttons[1, i].Text == currentPlayer.ToString() && buttons[2, i].Text == currentPlayer.ToString()) ||
+                    (buttons[0, 0].Text == currentPlayer.ToString() && buttons[1, 1].Text == currentPlayer.ToString() && buttons[2, 2].Text == currentPlayer.ToString()) ||
+                    (buttons[0, 2].Text == currentPlayer.ToString() && buttons[1, 1].Text == currentPlayer.ToString() && buttons[2, 0].Text == currentPlayer.ToString()))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool IsBoardFull()
+        {
+            foreach (Button button in buttons)
+            {
+                if (button.Text == "")
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public void ResetGame()
+        {
+            foreach (Button button in buttons)
+            {
+                button.Text = "";
+            }
+            currentPlayer = 'X';
+            label1.Text = "00:00:00";
+            seconds = 0;
+        }
+
+        public void timer1_tick()
+        {
+            TimeSpan time = TimeSpan.FromSeconds(seconds);
+            seconds++;
+            label1.Text = time.ToString(@"hh\:mm\:ss");
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe
+{
+    public partial class Form1 : Form
+    {
+        private Button[,] buttons;
+        private TicTacToeGame ticTacToeGame;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeGame();
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            this.Font = new Font(this.Font.FontFamily, this.Font.SizeInPoints * 125 / 96);
+            base.OnLoad(e);
+        }
+
+        private void InitializeGame()
+        {
+            buttons = new Button[3, 3];
+
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    buttons[row, col] = new Button();
+                    buttons[row, col].Size = new Size(120, 120);
+                    buttons[row, col].Location = new Point(col * 120, row * 120);
+                    buttons[row, col].Font = new Font("Arial", 48F, FontStyle.Bold);
+                    buttons[row, col].Click += Button_Click;
+
+                    Controls.Add(buttons[row, col]);
+                }
+            }
+
+            label1 = new Label();
+            label1.Text = "00:00:00";
+            label1.Location = new Point(151, 400);
+            Controls.Add(label1);
+
+            ticTacToeGame = new TicTacToeGame(buttons, label1);
+            timer1.Start();
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            ticTacToeGame.Button_Click((Button)sender);
+
+            if (ticTacToeGame.IsGameWon())
+            {
+                MessageBox.Show($"Player {ticTacToeGame.CurrentPlayer} wins!");
+                ticTacToeGame.ResetGame();
+            }
+            else if (ticTacToeGame.IsBoardFull())
+            {
+                MessageBox.Show("It's a draw!", label1.Text);
+                ticTacToeGame.ResetGame();
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ticTacToeGame.timer1_tick();
+        }
+    }
+}
